dbf190a975975c61e444450d1eacd63c68bf8a64
Ali jawwad <33836051+jawwad-ali@users.noreply.github.com>
2025-09-20 11:18:12 +0500

    added page.tsx

---
+++ app/page.tsx
@@ -0,0 +1,13 @@
+import { Navbar } from '@/components/navbar'
+import React from 'react'
+
+const Home = () => {
+  return (
+    <div>
+      <Navbar />
+      <h1>I am a homepage</h1>
+    </div>
+  )
+}
+
+export default Home
